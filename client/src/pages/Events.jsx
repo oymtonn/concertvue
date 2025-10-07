e100d33878199bb7cdd64be781ce6a9a47e092f7
Myo Aung <myoaungg213@gmail.com>
2025-10-06 22:15:54 -0700

    client

---
+++ client/src/pages/Events.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Events = () => {
+  return (
+    <section>
+      <h2>All Events</h2>
+      <p>Coming soon...</p>
+    </section>
+  );
+};
+
+export default Events;
